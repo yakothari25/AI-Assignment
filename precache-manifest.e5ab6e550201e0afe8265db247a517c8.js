989a0bbcc9d9ba8781abc3e6fa6441e98c9657e7
Yogesh <yakothari25@gmail.com>
2020-03-17 16:26:25 +0530

    Updates

---
+++ precache-manifest.e5ab6e550201e0afe8265db247a517c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67915c3a1624ae85c53c5cd8dac2d124",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41d610185890137da455",
+    "url": "/static/css/main.d038b80d.chunk.css"
+  },
+  {
+    "revision": "a52481d788ca6f374bf0",
+    "url": "/static/js/2.b09f1b41.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b09f1b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d610185890137da455",
+    "url": "/static/js/main.e3775ea6.chunk.js"
+  },
+  {
+    "revision": "5a5146acc643e0f57642",
+    "url": "/static/js/runtime-main.21bbe63e.js"
+  }
+]);
